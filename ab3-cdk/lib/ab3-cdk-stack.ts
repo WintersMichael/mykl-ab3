541d7dc0e183ef61cba0b64445dc60ba5ed2d83e
Michael Schuette <mykl@amazon.com>
2022-06-12 17:26:36 -0500

    Initial appsync

--- ab3-cdk/lib/ab3-cdk-stack.ts
+++ ab3-cdk/lib/ab3-cdk-stack.ts
@@ -4,14 +4,18 @@ import { AwsCustomResource, AwsCustomResourcePolicy, PhysicalResourceId } from '
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as logs from 'aws-cdk-lib/aws-logs';
+//import * as appsync from '@aws-cdk/aws-appsync-alpha';
+import * as appsync from 'aws-cdk-lib/aws-appsync';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 import productsData from '../products-test-data.json';
+const region = 'us-west-2';
 
 export class Ab3CdkStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
     
-    // Create Products table
+    // DDB Products table
     const productsTable = new dynamodb.Table(this, 'AB3ProductsTable', {
       partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
       sortKey: { name: 'sk', type: dynamodb.AttributeType.STRING },
@@ -47,9 +51,8 @@ export class Ab3CdkStack extends Stack {
         parameters: batchWriteCommand
       }
     });
-    //customPopulateDDB.node.addDependency(productsTable);
 
-    // Create Cognito UserPool
+    // Cognito UserPool
     const userPool = new cognito.UserPool(this, 'AB3UserPool', {
       accountRecovery: cognito.AccountRecovery.NONE,
       passwordPolicy: {
@@ -76,7 +79,7 @@ export class Ab3CdkStack extends Stack {
       preventUserExistenceErrors: true
     });
 
-    // Create user
+    // Create UserPool user
     const username = 'auser';
     const password = 'aoeuaoeu';
     const customCreateUser = new AwsCustomResource(this, 'customCreateUser', {
@@ -120,10 +123,114 @@ export class Ab3CdkStack extends Stack {
     });
     customSetUserPassword.node.addDependency(customCreateUser);
 
+    // AppSync
+    const api = new appsync.CfnGraphQLApi(this, 'AB3Api', {
+      name: 'AB3',
+      authenticationType: 'API_KEY',
+    });
+    const apiSchema = new appsync.CfnGraphQLSchema(this, 'Schema', {
+      apiId: api.attrApiId,
+      definition: `
+      type Product {
+        id: ID!
+        price: Float
+        name: String
+        description: String
+        description_mobile: String
+        aggregate_rating: Float
+      }
+      
+      type Query {
+        getAllProducts: [Product]
+        getProduct(id: ID!): Product
+      }
+      
+      schema {
+        query: Query
+      }
+      `
+    });
+    const apiDDBRole = new iam.Role(this, "ApiDDB", {
+      assumedBy: new iam.ServicePrincipal('appsync.amazonaws.com'),
+      managedPolicies: [
+        iam.ManagedPolicy.fromAwsManagedPolicyName('AmazonDynamoDBFullAccess'), //TODO better policy
+      ]
+    });
+    const apiDatasourceDDB = new appsync.CfnDataSource(this, "DDBDatasource", {
+      apiId: api.attrApiId,
+      name: 'ProductTable',
+      type: 'AMAZON_DYNAMODB',
+      serviceRoleArn: apiDDBRole.roleArn,
+      dynamoDbConfig: {
+        awsRegion: region,
+        tableName: productsTable.tableName
+      }
+    });
+    const resolverAllProducts = new appsync.CfnResolver(this, "AllProductsResolver", {
+      apiId: api.attrApiId,
+      dataSourceName: apiDatasourceDDB.name,
+      typeName: 'Query',
+      fieldName: 'getAllProducts',
+      requestMappingTemplate: `
+{
+  "version": "2017-02-28",
+  "operation": "Query",
+  "query" : {
+      "expression" : "id = :product",
+      "expressionValues" : {
+          ":product" : {"S": "product"}
+      }
+  },
+}
+      `,
+      responseMappingTemplate: `
+#set( $result = [] )
+#foreach( $item in $ctx.result.items )
+  $util.qr($result.add({
+      "id": $item.sk,
+      "price": $item.price,
+      "name": $item.listing.name,
+      "description": $item.listing.description,
+      "description_mobile": $item.listing.description_mobile,
+      "aggregate_rating": $item.listing.aggregate_rating
+    }))
+#end
+
+$util.toJson($result)
+      `
+    });
+    const resolverProductByID = new appsync.CfnResolver(this, "ResolverProductByID", {
+      apiId: api.attrApiId,
+      dataSourceName: apiDatasourceDDB.name,
+      typeName: 'Query',
+      fieldName: 'getProduct',
+      requestMappingTemplate: `
+{
+  "version": "2017-02-28",
+  "operation": "GetItem",
+  "key" : {
+    "id": {"S": "product"},
+    "sk": $util.dynamodb.toDynamoDBJson($ctx.args.id)
+  },
+}
+      `,
+      responseMappingTemplate: `
+$util.toJson({
+  "id": $ctx.result.sk,
+  "price": $ctx.result.price,
+  "name": $ctx.result.listing.name,
+  "description": $ctx.result.listing.description,
+  "description_mobile": $ctx.result.listing.description_mobile,
+  "aggregate_rating": $ctx.result.listing.aggregate_rating
+})
+      `
+    });
+
 
 
     new CfnOutput(this, 'ProductsTableName', { value: productsTable.tableName });
     new CfnOutput(this, 'CognitoPoolId', { value: userPool.userPoolId });
     new CfnOutput(this, 'CognitoClientId', { value: appClient.userPoolClientId });
+    new CfnOutput(this, 'AppSyncURL', { value: api.attrGraphQlUrl });
   }
 }
